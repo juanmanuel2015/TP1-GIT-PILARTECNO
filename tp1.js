dea64baf974d97720d51933ec4520f86961c2fc2
juan manuel andino <andino_2269@hotmail.com>
2024-05-16 20:45:42 -0300

    Resuelto Primer Ejercicio

---
+++ tp1.js
@@ -0,0 +1,27 @@
+/*  
+  Ejercicio 1: 
+  Implementar la función 'extractValues' que toma un objeto y retorna un array con todos los valores del objeto.
+  Ejemplo: 
+  extractValues({a: 1, b: 2, c: 3}) debería retornar [1, 2, 3]
+*/
+
+/*  
+  Ejercicio 2: 
+  Implementar la función 'arrayToLengthCounters' que toma un array de strings y retorna un objeto donde cada elemento
+  del array se convierte en una clave del objeto y el valor es la longitud de cada string.
+  Ejemplo: 
+  arrayToLengthCounters(['gato', 'perro', 'elefante']) debería retornar {gato: 4, perro: 5, elefante: 8}
+*/
+function extractValues(data) {
+    function extractValues(data) {
+      return Object.values(data);
+    }
+  }
+
+/*  
+  Ejercicio 3: 
+  Implementar la función 'findKeysInCommon' que toma dos objetos y retorna un array con los nombres de las 
+  propiedades que tienen en común.
+  Ejemplo:
+  findKeysInCommon({a: 1, b: 2, c: 3}, {b: 4, c: 5, d: 6}) debería retornar ['b', 'c']
+*/
